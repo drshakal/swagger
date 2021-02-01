fa2764da37d85ba0fb5d664267e531a2af545f97
ajay <ajayborgave.ab@gmail.com>
2021-02-01 20:30:57 +0530

    test the route....

--- demo-1/src/main/java/in/co/dhdigital/controllers/restController.java
+++ demo-1/src/main/java/in/co/dhdigital/controllers/restController.java
@@ -2,8 +2,12 @@ package in.co.dhdigital.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.co.dhdigital.models.RetriveOnlineStatementRequest;
+import in.co.dhdigital.models.RetriveOnlineStatementResponce;
 
 @RestController
 @RequestMapping("/api")
@@ -15,6 +19,28 @@ public class restController {
 		String msg;
 		msg="Hello "+name+"....!";		
 		return msg;
+		
+	}
+	
+	
+	@GetMapping("/RetrieveOnlineStatement")
+	public RetriveOnlineStatementResponce retriveOnlineStatement(@RequestBody RetriveOnlineStatementRequest req )
+	{
+		RetriveOnlineStatementResponce res = null;
+		
+		if (req.getAccountNumber().isEmpty()||req.getExemptCharge().isEmpty()||req.getFromDate().isEmpty()||req.getRestartAmount().isEmpty()||req.getRestartRecNo().isEmpty()||req.getToDate().isEmpty())
+		{
+			res.setSuccess("FAIL");
+			res.setFault("902 : Invalid Inputs");
+		}
+		else
+		{
+			res.setSuccess("SUCCESS");
+			res.setFault("NULL");
+		}
+			
+			
+		return res;
 		
 	}
 	
---
+++ demo-1/src/main/java/in/co/dhdigital/models/RetriveOnlineStatementResponce.java
@@ -0,0 +1,21 @@
+package in.co.dhdigital.models;
+
+public class RetriveOnlineStatementResponce {
+
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	public String getFault() {
+		return fault;
+	}
+	public void setFault(String fault) {
+		this.fault = fault;
+	}
+	String success;
+	String fault;
+	
+	
+}
